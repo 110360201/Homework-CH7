a6f9fa407249e153c57cbfd44a50039a0862bf5f
110360201 <t110360201@ntut.org.tw>
2021-12-23 14:44:04 +0800

    first

---
+++ 110360201_蔡侑恩/P18/source/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	FILE *in, *out;
+	int cnt;
+	char str[100];
+	fopen_s(&in, "welcome.txt", "r");
+	fopen_s(&out, "output.txt", "w");
+	while (!feof(in))
+	{
+		cnt = fscanf_s(in, "%s", str, 100);
+		if (cnt > 0)
+		{
+			fprintf(out, "%s\n", str);
+		}
+	}
+		fclose(in);
+		fclose(out);
+		system("pause");
+		return 0;
+}
